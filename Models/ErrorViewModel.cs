3f84a314a4764c87abde52d50b982f16b983959c
Angelos Petropoulos <angelpe@microsoft.com>
2020-06-13 03:47:05 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Example_Redis_Cache.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
